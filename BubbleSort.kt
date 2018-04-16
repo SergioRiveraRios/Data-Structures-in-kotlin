34e46f062614b4b56416357bcd7b9f22a60daa9a
Sergio Rivera <33271719+SergioRiveraRios@users.noreply.github.com>
2018-04-15 23:57:21 -0600

    Update BubbleSort.kt

--- BubbleSort.kt
+++ BubbleSort.kt
@@ -1,7 +1,9 @@
-fun main(ar:Array<String>){
-   var A= arrayOf<Int>(1,5,3,43,2,3,4,65,45,7)
+fun BubbleSort(A:Array<Int>){
+    for(l in A){
+        print(l.toString()+",")
+    }
     for(i in 0..A.size-1){
-        for(j in 0..A.size-3){
+        for(j in 0..A.size-2){
             if(A[j]>A[j+1]){
                 var temp=A[j]
                 A[j]=A[j+1]
@@ -9,7 +11,12 @@ fun main(ar:Array<String>){
             }
         }
     }
+    println("")
     for(v in A){
         print(v.toString()+",")
-        }
+    }
+}
+fun main(ar:Array<String>){
+   var b= arrayOf<Int>(6,5,3,43,2,3,4,65,45,12,447,1)
+   BubbleSort(b)
 }
