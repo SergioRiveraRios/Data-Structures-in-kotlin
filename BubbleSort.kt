7d24185c77c6062cf835eec64cc50ea865b1a787
Sergio <seerriverari@ittepic.edu.mx>
2018-04-16 00:04:46 -0600

    Added some stuff

--- BubbleSort.kt
+++ BubbleSort.kt
@@ -1,7 +1,15 @@
-fun main(ar:Array<String>){
-   var A= arrayOf<Int>(1,5,3,43,2,3,4,65,45,7)
+/*  Starts in the beginning of the array and swapping the first two 
+    elements only if the first element is greater than the second element
+    This comparison is then moved onto the next pair and so on and so forth
+    This is done until the array is completely sorted
+
+ */
+fun BubbleSort(A:Array<Int>){
+    for(l in A){
+        print(l.toString()+",")
+    }
     for(i in 0..A.size-1){
-        for(j in 0..A.size-3){
+        for(j in 0..A.size-2){
             if(A[j]>A[j+1]){
                 var temp=A[j]
                 A[j]=A[j+1]
@@ -9,7 +17,12 @@ fun main(ar:Array<String>){
             }
         }
     }
+    println("")
     for(v in A){
         print(v.toString()+",")
-        }
+    }
+}
+fun main(ar:Array<String>){
+   var b= arrayOf<Int>(6,5,3,43,2,3,4,65,45,12,447,1)
+   BubbleSort(b)
 }
